99917c622079ce552f6bd086a2a04a429fd3643f
Mike Galvin <mikefgalvin@gmail.com>
2024-09-13 12:10:19 -0700

    remove ignored files

--- docs/.docusaurus/client-modules.js
+++
@@ -1,5 +0,0 @@
-export default [
-  require("/Users/mike/code/grokit/paranet/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/mike/code/grokit/paranet/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/mike/code/grokit/paranet/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-];
